7223c92b277964d51d8b5a28afeef7ed3f0c68d6
Leonhardiv94 <leonhardiv94@gmail.com>
2026-08-08 22:17:21 -0500

    Fix Phaser canvas graphics path drawing in generateProceduralTempleTexture to ensure clean scene initialization and depth layer sorting

--- client/src/scenes/GameScene.ts
+++ client/src/scenes/GameScene.ts
@@ -2104,58 +2104,56 @@ export class GameScene extends Phaser.Scene {
     const w = 260;
     const h = 230;
 
+    const drawFilledPoly = (pts: number[], color: number, alpha: number = 1) => {
+      g.fillStyle(color, alpha);
+      g.beginPath();
+      g.moveTo(pts[0], pts[1]);
+      for (let i = 2; i < pts.length; i += 2) {
+        g.lineTo(pts[i], pts[i + 1]);
+      }
+      g.closePath();
+      g.fillPath();
+    };
+
+    const drawStrokedPoly = (pts: number[], color: number, width: number = 1.5, alpha: number = 0.9) => {
+      g.lineStyle(width, color, alpha);
+      g.beginPath();
+      g.moveTo(pts[0], pts[1]);
+      for (let i = 2; i < pts.length; i += 2) {
+        g.lineTo(pts[i], pts[i + 1]);
+      }
+      g.closePath();
+      g.strokePath();
+    };
+
     // 1. Sombra suave de la base del templo en el suelo
     g.fillStyle(0x0f172a, 0.22);
     g.fillTriangle(130, 205, 10, 140, 250, 140);
     g.fillTriangle(130, 205, 250, 140, 130, 90);
 
     // 2. Base de Piedra Tallada (Muro Izquierdo Sombreado)
-    g.fillStyle(0x64748b, 1);
-    g.fillPolygon([
-      { x: 30, y: 140 },
-      { x: 130, y: 190 },
-      { x: 130, y: 135 },
-      { x: 30, y: 85 }
-    ]);
+    drawFilledPoly([30, 140, 130, 190, 130, 135, 30, 85], 0x64748b);
 
     // 3. Base de Piedra Tallada (Muro Derecho Iluminado - Fachada Principal)
-    g.fillStyle(0x94a3b8, 1);
-    g.fillPolygon([
-      { x: 130, y: 190 },
-      { x: 230, y: 140 },
-      { x: 230, y: 85 },
-      { x: 130, y: 135 }
-    ]);
+    drawFilledPoly([130, 190, 230, 140, 230, 85, 130, 135], 0x94a3b8);
 
     // Detalle de Bloques de Piedra en la fachada
     g.lineStyle(1.5, 0x475569, 0.7);
     g.beginPath();
     g.moveTo(30, 115); g.lineTo(130, 165); g.lineTo(230, 115);
-    g.moveTo(30, 98); g.lineTo(130, 148); g.lineTo(230, 98);
+    g.moveTo(30, 98);  g.lineTo(130, 148); g.lineTo(230, 98);
     g.strokePath();
 
     // 4. Escaleras de Piedra de Entrada (Orientadas hacia Abajo a la Derecha)
-    g.fillStyle(0xcbd5e1, 1);
-    g.fillPolygon([
-      { x: 158, y: 176 }, { x: 192, y: 159 }, { x: 200, y: 163 }, { x: 166, y: 180 }
-    ]);
-    g.fillPolygon([
-      { x: 166, y: 180 }, { x: 200, y: 163 }, { x: 208, y: 167 }, { x: 174, y: 184 }
-    ]);
-    g.fillStyle(0x94a3b8, 1);
-    g.fillPolygon([
-      { x: 174, y: 184 }, { x: 208, y: 167 }, { x: 216, y: 171 }, { x: 182, y: 188 }
-    ]);
+    drawFilledPoly([158, 176, 192, 159, 200, 163, 166, 180], 0xcbd5e1);
+    drawFilledPoly([166, 180, 200, 163, 208, 167, 174, 184], 0xcbd5e1);
+    drawFilledPoly([174, 184, 208, 167, 216, 171, 182, 188], 0x94a3b8);
 
     // 5. Arco y Puerta de Madera de Entrada (Fachada Derecha, mirando hacia Abajo-Derecha)
-    g.fillStyle(0xe2e8f0, 1);
-    g.fillPolygon([
-      { x: 152, y: 154 }, { x: 188, y: 136 }, { x: 188, y: 104 }, { x: 152, y: 122 }
-    ]);
-    g.fillStyle(0x92400e, 1);
-    g.fillPolygon([
-      { x: 155, y: 151 }, { x: 185, y: 136 }, { x: 185, y: 109 }, { x: 155, y: 124 }
-    ]);
+    drawFilledPoly([152, 154, 188, 136, 188, 104, 152, 122], 0xe2e8f0);
+    drawFilledPoly([155, 151, 185, 136, 185, 109, 155, 124], 0x92400e);
+
+    // Remaches de Hierro de la Puerta
     g.fillStyle(0x334155, 1);
     g.fillCircle(162, 143, 1.5);
     g.fillCircle(178, 135, 1.5);
@@ -2163,31 +2161,10 @@ export class GameScene extends Phaser.Scene {
     g.fillCircle(178, 121, 1.5);
 
     // 6. Techo de Paja Dorada (Thatched Roof)
-    g.fillStyle(0xb45309, 1);
-    g.fillPolygon([
-      { x: 20, y: 85 },
-      { x: 130, y: 140 },
-      { x: 130, y: 45 },
-      { x: 20, y: 20 }
-    ]);
-
-    g.fillStyle(0xf59e0b, 1);
-    g.fillPolygon([
-      { x: 130, y: 140 },
-      { x: 240, y: 85 },
-      { x: 240, y: 20 },
-      { x: 130, y: 45 }
-    ]);
-
-    g.fillStyle(0x78350f, 1);
-    g.fillPolygon([
-      { x: 16, y: 18 }, { x: 130, y: 42 }, { x: 244, y: 18 }, { x: 130, y: 38 }
-    ]);
-
-    g.fillStyle(0xfbbf24, 1);
-    g.fillPolygon([
-      { x: 130, y: 136 }, { x: 236, y: 85 }, { x: 236, y: 24 }, { x: 130, y: 48 }
-    ]);
+    drawFilledPoly([20, 85, 130, 140, 130, 45, 20, 20], 0xb45309);
+    drawFilledPoly([130, 140, 240, 85, 240, 20, 130, 45], 0xf59e0b);
+    drawFilledPoly([16, 18, 130, 42, 244, 18, 130, 38], 0x78350f);
+    drawFilledPoly([130, 136, 236, 85, 236, 24, 130, 48], 0xfbbf24);
 
     // 7. Torre / Campanario Superior de Piedra
     g.fillStyle(0x64748b, 1);
@@ -2197,16 +2174,14 @@ export class GameScene extends Phaser.Scene {
     g.fillStyle(0x78350f, 1);
     g.fillTriangle(114, 20, 142, 20, 130, 6);
 
+    // Banderín / Estandarte Dorado Flameando
     g.lineStyle(2, 0x475569, 1);
     g.lineBetween(130, 6, 130, -6);
     g.fillStyle(0xf59e0b, 1);
     g.fillTriangle(130, -6, 148, -2, 130, 2);
 
-    g.lineStyle(1.5, 0x334155, 0.9);
-    g.strokePolygon([
-      { x: 30, y: 140 }, { x: 130, y: 190 }, { x: 230, y: 140 }, { x: 230, y: 85 },
-      { x: 240, y: 20 }, { x: 130, y: 45 }, { x: 20, y: 20 }, { x: 30, y: 85 }
-    ]);
+    // Contornos limpios idénticos al estilo de los árboles y rocas del juego
+    drawStrokedPoly([30, 140, 130, 190, 230, 140, 230, 85, 240, 20, 130, 45, 20, 20, 30, 85], 0x334155, 1.5, 0.9);
 
     g.generateTexture('temple-building', w, h);
     g.destroy();
